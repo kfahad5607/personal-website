ee97f4ac1e2cd84246e2223cda1993032be49c17
Fahad Khan <kfahad5607@gmail.com>
2024-09-26 22:15:55 +0530

    Added spotlight

---
+++ app/layout-client.tsx
@@ -0,0 +1,30 @@
+"use client"
+import Link from 'next/link'
+import React, { MouseEvent, useRef, useState } from 'react'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const LayoutClient = ({ children }: Props) => {
+  const [spotlightStyle, setSpotlightStyle] = useState({
+    background: 'radial-gradient(600px at 0px 0px, rgba(29, 78, 216, 0.15), transparent 80%)'
+  })
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    setSpotlightStyle({
+      background: `radial-gradient(600px at ${e.pageX}px ${e.pageY}px, rgba(29, 78, 216, 0.15), transparent 80%)`
+    })
+  }
+
+  return (
+    <div onMouseMove={handleMouseMove}>
+      <div style={spotlightStyle} className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" ></div>
+      <div className="min-h-screen max-w-screen-xl mx-auto px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <Link href="#content" className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">Skip to Content</Link>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default LayoutClient
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import Link from 'next/link'
 import localFont from 'next/font/local'
 import "./globals.css";
+import LayoutClient from "./layout-client";
 
 const inter = localFont({
   src: '../public/fonts/Inter-4.0/web/InterVariable.woff2',
@@ -23,12 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} scroll-smooth`}>
       <body
-        className={`bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
+        className={`relative bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
       >
-        <div className="min-h-screen max-w-screen-xl mx-auto px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
-          <Link href="#content" className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">Skip to Content</Link>
+        <LayoutClient>
           {children}
-        </div>
+        </LayoutClient>
       </body>
     </html>
   );
